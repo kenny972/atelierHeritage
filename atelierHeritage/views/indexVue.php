d36c7f7083c92ba2e65fc82cfdf81999b9e4766d
Kenny <kennygenly@gmail.com>
2017-10-26 08:10:56 +0200

    commit

---
+++ atelierHeritage/views/indexVue.php
@@ -0,0 +1,10 @@
+<?php
+  include("template/header.php")
+ ?>
+<div class="container">
+  <p>This is the index view</p>
+</div>
+
+ <?php
+   include("template/footer.php")
+  ?>
---
+++ atelierHeritage/views/template/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Your site title</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <link rel="stylesheet" href="../assets/css/main.css">
+  </head>
+  <body>
+    <header class="jumbotron jumbotron-fluid mb-5 mainColor colorWhite">
+      <div class="container">
+        <h1 class="display-3">L'héritage en PHP</h1>
+        <p class="lead">atelier Simplon</p>
+      </div>
+    </header>
